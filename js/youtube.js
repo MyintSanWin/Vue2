45dd802a291cd254c21699ad184177b644991815
MyintSanWin <jackmee24680@gmail.com>
2021-12-13 14:27:01 +0630

    Toggle Episode List

---
+++ js/youtube.js
@@ -0,0 +1,14 @@
+document.getElementById("button").onclick = function () {
+  document.getElementById("confirm").innerHTML =
+    "order placed,Checke email for confirmation ";
+  document.getElementById("button").style.display = "none";
+};
+//var username = prompt();
+username = 5;
+username = "hello";
+console.log(username);
+var age = 30;
+var newage = age;
+var message = "newage" + "  " + newage;
+console.log(age);
+console.log(message);

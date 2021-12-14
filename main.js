784c5ee4c6823ee9d9b83361cd3a51e5b46daa61
MyintSanWin <jackmee24680@gmail.com>
2021-12-14 10:30:55 +0630

    Components within components

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+Vue.component("task-list", {
+  template: '
+  <div> 
+  <task v-for="task in tasks">{{task.task}}</task>
+  </div>',
+
+  data() {
+    return {
+      tasks: [
+        { task: "Go to the store", complete: true },
+        { task: "Go to the bank", complete: false },
+        { task: "Go to the work", complete: true },
+        { task: "Go to the farm", complete: true },
+        { task: "Go to the email", complete: false },
+      ],
+    };
+  },
+});
 Vue.component("task", {
   template: "<li><slot></slot></li>",
 });
